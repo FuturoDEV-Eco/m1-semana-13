8fe3b650dd04c8f902d497146327d9b7fa77588d
danielly-kammers <daniellyckammers@thomsonreuters.com>
2024-04-16 14:39:50 -0300

    fix: 🐛 remove unecessary comments

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,6 @@ import ProductContextProvider from "./contexts/products";
 import CounterHook from "./components/pages/CounterHook";
 
 function App() {
- // preciso do estado em que o counter está e uma função que atualize o counter
- // const [count, increment, incrementTwo] = useCounter()
- // const [product, incrementProduct, incrementTwoproducts] = useCounter()
-
- // se eu colocar codigo js aqui vai executar novamente em todas as renderizações
-
  const router = createBrowserRouter([
   {
    path: "/",
@@ -33,31 +27,6 @@ function App() {
    <RouterProvider router={router} />
   </ProductContextProvider>
  );
-}
-
-{
- /* <h1>M1 - Semana 13</h1>
-      <div className="card">
-        <h1>{count}</h1>
-        <button onClick={() => increment()}>
-        Adicionar + 1 ao contador
-        </button>
-
-        <button onClick={() => incrementTwo()}>
-          Adicionar + 2 ao contador
-        </button>
-      </div>
-
-      <div className="card">
-        <h3>Carrinho de compras {product}</h3>
-        <button onClick={() => incrementProduct()}>
-        Adicionar 1 item
-        </button>
-
-        <button onClick={() => incrementTwoproducts()}>
-          Adicionar 2 items
-        </button>
-      </div> */
 }
 
 export default App;
